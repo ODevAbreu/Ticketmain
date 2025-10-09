558008c1e2d4453de8394cfe69c7a54006463e0a
Logan Borges <logan.bernardes@pucpr.edu.br>
2025-10-08 21:16:58 -0300

    Adicionando Security - Rogerio de Abreu

---
+++ src/main/java/br/pucpr/projetowebservice/model/Usuario.java
@@ -0,0 +1,15 @@
+package br.pucpr.projetowebservice.model;
+
+import lombok.Data;
+
+@Data
+public class Usuario {
+
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+
+}
